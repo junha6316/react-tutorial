f04d3440b06219fc5a700a33fd954703ff0658cb
junha6316 <junha6316@gmail.com>
2022-05-01 19:25:12 +0900

    추가: React 기초

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,9 +1,24 @@
-
+import Button  from "./Button";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue(current => current +1)
+  const onChange = (event) => setKeyword(event.target.value)
+  const apiCall =()=>  console.log("call an api")
+  
+  useEffect(apiCall,[])
+  useEffect(()=>{
+    if(keyword !== "" && keyword.length >= 5){
+      console.log("search for", keyword)
+    }
+  }, [keyword])
   return (
     <div className="App">
-      <h1>Welcom back!</h1>
+    <input type="text" placeholder="Search here.." onChange={onChange}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
