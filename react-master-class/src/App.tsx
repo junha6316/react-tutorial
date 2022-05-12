63a88eca4a0d85e7b2e5a71063be46601fd7d294
junha6316 <junha6316@gmail.com>
2022-05-12 22:56:05 +0900

    추가: createGlobalStyle로 css reset

--- react-master-class/src/App.tsx
+++ react-master-class/src/App.tsx
@@ -1,76 +1,61 @@
-import React from 'react';
-import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import Router from './Router';
 
-// const Title = styled.h1`
-//   color: ${(props) => props.theme.textColor}
-// `
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   background-color:${props => props.theme.backgroundColor}
-// `
-// const animation = keyframes`
-//   0% {
-//     transform: rotate(0deg);
-//     border-radius:0px;
-//   }
-//   50%{
-//     transform: rotate(180deg);
-//     border-radius: 30px
-//   }
-//   100%{
-//     transform: rotate(360deg);
-//     border-radius: 50px
-//   }
-
-// `
-
-// const Emoji = styled.span`
-//   font-size:16px;
-// `
-
-// const Box = styled.div`
-//   background-color: ${props => props.bgColor};
-//   width: 100px;
-//   height: 100px;
-//   animation:${animation} 1s linear infinite;
-//   display:flex;
-//   justify-content:center;
-//   align-items:center;
-//   ${Emoji}{
-//     &:hover{
-//       font-size:50px;
-//     }
-//     &:active{
-//       opacity:0;
-//     }
-//   }
-// `
-
-// const Btn = styled.button`
-//   color: white;
-//   background-color:tomato;
-//   border: 0;
-//   border-radius:15px;
-
-// `
-
-// const Input = styled.input.attrs({required:true})`
-//   background-color: tomato;
-// `
-
+const GlobalStyle = createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, menu, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, main, menu, nav, section {
+    display: block;
+  }
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+      display: none;
+  }
+  body {
+    line-height: 1;
+  }
+  menu, ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
 function App() {
-  const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-  `;
-  const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};
-  `;
   return (
-    <Container>
-      <H1 />
-    </Container>
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
   );
 }
-
 export default App;
