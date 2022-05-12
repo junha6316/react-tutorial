c1e83df08430ca4ae55dc5c83878422b5a50b891
junha6316 <junha6316@gmail.com>
2022-05-12 22:35:54 +0900

    추가: theme 기본으로 변경

--- react-master-class/src/theme.ts
+++ react-master-class/src/theme.ts
@@ -1,5 +1,11 @@
 import { DefaultTheme } from 'styled-components';
 
+export const theme: DefaultTheme = {
+  textColor: 'white',
+  bgColor: 'black',
+  btnColor: 'tomato',
+};
+
 export const lightTheme: DefaultTheme = {
   textColor: 'white',
   bgColor: 'black',
