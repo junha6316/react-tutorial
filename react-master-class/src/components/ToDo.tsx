e3a09d234a190494fc157991222b062fbb319c88
parkjunhada <junha6316@gmail.com>
2022-05-19 20:02:00 +0900

    수정: toDo 버튼에 따른 변경

--- react-master-class/src/components/ToDo.tsx
+++ react-master-class/src/components/ToDo.tsx
@@ -1,13 +1,43 @@
 import { IToDo } from '../index.d';
 import { toDoState } from '../atoms';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
 
-function ToDo({ id, text }: IToDo) {
+function ToDo({ id, text, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+
+    setToDos((toDos) => {
+      const targetIdx = toDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...toDos.slice(0, targetIdx),
+        newToDo,
+        ...toDos.slice(targetIdx + 1),
+      ];
+    });
+  };
   return (
     <li key={id}>
       {text}
-      <button>DONE</button>
-      <button>DOING</button>
-      <button>TO DO</button>
+      {category !== 'DOING' && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== 'TO_DO' && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== 'DONE' && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
