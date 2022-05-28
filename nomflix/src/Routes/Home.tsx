7ef80ec7ba1262d3937813281a62f05b985f6991
parkjunhada <junha6316@gmail.com>
2022-05-28 23:45:59 +0900

    8.2 Header part Two

--- nomflix/src/Routes/Home.tsx
+++ nomflix/src/Routes/Home.tsx
@@ -1,5 +1,9 @@
 function Home() {
-  return <div style={{ backgroundColor: 'black', height: '200vh' }}></div>;
+  return (
+    <div style={{ backgroundColor: 'white', height: '200vh' }}>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
