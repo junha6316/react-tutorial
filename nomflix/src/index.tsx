3b5c96ed74a0f8a6cb21ca1c863c5c6fecf6f716
parkjunhada <junha6316@gmail.com>
2022-05-28 16:10:45 +0900

    추가: 기본 세팅

--- nomflix/src/index.tsx
+++ nomflix/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { GlobalStyle } from './globalStyle';
 import App from './App';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
